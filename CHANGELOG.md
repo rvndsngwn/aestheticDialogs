02a3b9b7f9dd198f2e26484ce24ea10061688a76
arvindsangwan <rvndsngwn@gmail.com>
2021-09-13 11:13:38 +0530

    Move to null safety

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+## [0.0.13] - February 16, 2021
+
+* Fix Gradle bug
+
+## [0.0.12+1] - January 02, 2021
+
+* Add AndroidX.
+* Fix Bugs
+
+## [0.0.11+1] - December 31, 2020
+
+* New Release.
+
+## [0.0.11] - Novembre 24, 2020
+
+* Fix Install Bugs.
+
+## [0.0.1] - Novembre 03, 2020
+
+* Initial release.
---
+++ example/lib/main.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+
+import 'package:aesthetic_dialogs/aesthetic_dialogs.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> buildDialog() async {
+    AestheticDialogs.showDialog(
+        title: "Ma Boite",
+        message: "Bonjour",
+        cancelable: true,
+        darkMode: false,
+        dialogAnimation: DialogAnimation.IN_OUT,
+        dialogGravity: DialogGravity.CENTER,
+        dialogStyle: DialogStyle.EMOJI,
+        dialogType: DialogType.INFO,
+        duration: 5000);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('AestheticDialogs Example Flutter'),
+        ),
+        body: Center(
+          child: MaterialButton(
+            onPressed: buildDialog,
+            child: Text("Open Dialog"),
+          ),
+        ),
+      ),
+    );
+  }
+}
